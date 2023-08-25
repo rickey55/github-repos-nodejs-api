cad63c942e5fea379289f13c9362e037f077736a
rickey55 <rickeysue@gmail.com>
2023-08-25 23:59:59 +0800

    revert: feat(pencil): add 'graphiteWidth' option

--- index.js
+++ index.js
@@ -2,26 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 8081;
+var testAPIRouter = require("./api/routes/testAPI");
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
+app.use("/testAPI", testAPIRouter);
 
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+app.get("/", function (req, res) {
+  res.send("hello world");
 });
 
 app.listen(PORT, () => {
